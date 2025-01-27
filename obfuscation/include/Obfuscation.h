ecb3a1fbdb0def67fad2d6f81ca9a4fa8e2ef93c
PPKunOfficial <2967630235@qq.com>
2025-01-27 15:26:17 +0800

    init

---
+++ obfuscation/include/Obfuscation.h
@@ -0,0 +1,35 @@
+#ifndef _OBFUSCATION_H_
+#define _OBFUSCATION_H_
+
+#include "llvm/Support/Timer.h"
+#include "include/AntiClassDump.h"
+#include "include/AntiDebugging.h"
+#include "include/AntiHook.h"
+#include "include/BogusControlFlow.h"
+#include "include/ConstantEncryption.h"
+#include "include/CryptoUtils.h"
+#include "include/Flattening.h"
+#include "include/FunctionCallObfuscate.h"
+#include "include/FunctionWrapper.h"
+#include "include/IndirectBranch.h"
+#include "include/Split.h"
+#include "include/StringEncryption.h"
+#include "include/Substitution.h"
+#include "llvm/Transforms/Utils/BasicBlockUtils.h"
+#include "llvm/Transforms/Utils/ModuleUtils.h"
+
+namespace llvm {
+
+class ObfuscationPass : public PassInfoMixin<ObfuscationPass> {
+public:
+  ObfuscationPass() {}
+  PreservedAnalyses run(Module &M, ModuleAnalysisManager &MAM);
+  static bool isRequired() { return true; }
+};
+
+ModulePass *createObfuscationLegacyPass();
+void initializeObfuscationPass(PassRegistry &Registry);
+
+} // namespace llvm
+
+#endif
